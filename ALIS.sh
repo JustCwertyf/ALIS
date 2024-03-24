f8e412404fc7620904712dcdb7edcb07513d50e9
Арсений Фольц <158209133+JustCwertyf@users.noreply.github.com>
2024-03-24 17:00:45 +0500

    Update ALIS.sh

--- ALIS.sh
+++ ALIS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Arch Linux initial setup by JustCwertyf"
-sudo pacman -Syu -y
+sudo pacman -Syu --noconfirm
 echo "Choose, which graphics drivers do you need:"
 echo "1. NVIDIA drivers"
 echo "2. AMD drivers "
@@ -11,42 +11,42 @@ read choice
 
 case $choice in
     1)
-        sudo pacman -S nvidia-dkms -y
-        sudo pacman -S nvidia-utils -y
+        sudo pacman -S nvidia-dkms --noconfirm
+        sudo pacman -S nvidia-utils --noconfirm
         ;;
     2)
-        sudo pacman -S mesa -y
-        sudo pacman -S amdvlk -y
+        sudo pacman -S mesa --noconfirm
+        sudo pacman -S amdvlk --noconfirm
         ;;
     3)
-        sudo pacman -S mesa -y
-        sudo pacman -S vulkan-intel -y
-        sudo pacman -S xf86-video-intel -y
+        sudo pacman -S mesa --noconfirm
+        sudo pacman -S vulkan-intel --noconfirm
+        sudo pacman -S xf86-video-intel --noconfirm
         ;;
     4)
-        sudo pacman -S mesa -y
-        sudo pacman -S amdvlk -y
-        sudo pacman -S vulkan-intel -y
-        sudo pacman -S xf86-video-intel -y
-        sudo pacman -S nvidia-dkms -y
-        sudo pacman -S nvidia-utils -y
+        sudo pacman -S mesa --noconfirm
+        sudo pacman -S amdvlk --noconfirm
+        sudo pacman -S vulkan-intel --noconfirm
+        sudo pacman -S xf86-video-intel --noconfirm
+        sudo pacman -S nvidia-dkms --noconfirm
+        sudo pacman -S nvidia-utils --noconfirm
         ;;
     *)
         echo "Uncorrect. Chose number 1-4."
         ;;
 esac
-sudo pacman -S archlinux-keyring
-sudo pacman -S --needed git base-devel -y
-sudo pacman -Sy git -y
-sudo pacman -S make -y
-sudo pacman -S linux-headers
-sudo pacman -S firefox -y
-sudo pacman -S noto-fonts-emoji noto-fonts ttf-liberation -y
-sudo pacman -S pulseaudio -y
-sudo pacman -S blueman -y
+sudo pacman -S archlinux-keyring --noconfirm
+sudo pacman -S --needed git base-devel --noconfirm
+sudo pacman -Sy git --noconfirm
+sudo pacman -S make --noconfirm
+sudo pacman -S linux-headers --noconfirm
+sudo pacman -S firefox --noconfirm
+sudo pacman -S noto-fonts-emoji noto-fonts ttf-liberation --noconfirm
+sudo pacman -S pulseaudio --noconfirm
+sudo pacman -S blueman --noconfirm
 sudo systemctl enable bluetooth
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si --noconfirm
 echo "Your pc will be rebooted"
 sudo reboot
